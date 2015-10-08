195b587d40f203fd5a9639127d517c11517b6178
Ryan Arana <ryan@ryanarana.com>
2015-10-07 21:43:27 -0700

    Copy (and update) tests from WatchKitWorkshop

--- RoShamBo/RoShamBoTests/RoShamBoTests.swift
+++ RoShamBo/RoShamBoTests/RoShamBoTests.swift
@@ -21,16 +21,63 @@ class RoShamBoTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test__game_with_same_choices_should_be_a_draw() {
+        let r = Game(choices: .Rock, .Rock)
+        XCTAssert(r.draw, "Rock vs Rock should be a draw")
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
+    func test__game_with_different_choices_should_not_be_a_draw() {
+        let r = Game(choices: .Rock, .Paper)
+        XCTAssert(!r.draw, "Rock vs Paper should not be a draw")
     }
     
+    var choiceUnderTest: Choice = .Rock
+    func vs (c: Choice) -> Game {
+        return Game(choices: choiceUnderTest, c)
+    }
+    
+    func test__rock_should_interact_with_other_choices_correctly() {
+        choiceUnderTest = .Rock
+        
+        XCTAssert(vs(.Paper).winner != .Rock, "Rock should lose to Paper")
+        XCTAssert(vs(.Spock).winner != .Rock, "Rock should lose to Spock")
+        XCTAssert(vs(.Scissors).winner == .Rock, "Rock should defeat Scissors")
+        XCTAssert(vs(.Lizard).winner == .Rock, "Rock should defeat Lizard")
+    }
+    
+    func test__paper_should_interact_with_other_choices_correctly() {
+        choiceUnderTest = .Paper
+        
+        XCTAssert(vs(.Scissors).winner != .Paper, "Paper should lose to Scissors")
+        XCTAssert(vs(.Lizard).winner != .Paper, "Paper should lose to Lizard")
+        XCTAssert(vs(.Rock).winner == .Paper, "Paper should defeat Rock")
+        XCTAssert(vs(.Spock).winner == .Paper, "Paper should defeat Spock")
+    }
+    
+    func test__scissors_should_interact_with_other_choices_correctly() {
+        choiceUnderTest = .Scissors
+        
+        XCTAssert(vs(.Rock).winner != .Scissors, "Scissors should lose to Rock")
+        XCTAssert(vs(.Spock).winner != .Scissors, "Scissors should lose to Spock")
+        XCTAssert(vs(.Paper).winner == .Scissors, "Scissors should defeat Paper")
+        XCTAssert(vs(.Lizard).winner == .Scissors, "Scissors should defeat Lizard")
+    }
+    
+    func test__lizard_should_interact_with_other_choices_correctly() {
+        choiceUnderTest = .Lizard
+        
+        XCTAssert(vs(.Rock).winner != .Lizard, "Lizard should lose to Rock")
+        XCTAssert(vs(.Scissors).winner != .Lizard, "Lizard should lose to Scissors")
+        XCTAssert(vs(.Paper).winner == .Lizard, "Lizard should defeat Paper")
+        XCTAssert(vs(.Spock).winner == .Lizard, "Lizard should defeat Spock")
+    }
+    
+    func test__spock_should_interact_with_other_choices_correctly() {
+        choiceUnderTest = .Spock
+        
+        XCTAssert(vs(.Paper).winner != .Spock, "Spock should lose to Paper")
+        XCTAssert(vs(.Lizard).winner != .Spock, "Spock should lose to Lizard")
+        XCTAssert(vs(.Scissors).winner == .Spock, "Spock should defeat Scissors")
+        XCTAssert(vs(.Rock).winner == .Spock, "Spock should defeat Rock")
+    }
 }
